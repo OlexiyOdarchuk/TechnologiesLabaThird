85050f059b9222da064af697f01792a458d26c88
iShawyha <shawyhaf@gmail.com>
2025-09-29 15:29:40 +0300

    Працює краще, витягнув список операцій за цикл

--- main.cpp
+++ main.cpp
@@ -14,16 +14,17 @@ int main(){
     bool isCreate = false;
     int* arr = new int[ARR_LENGHT];
     char chose;
+    std::cout << "ОПЕРАЦІЇ З МАСИВАМИ: " << std::endl << std::endl;
+    std::cout << "1) Ввести значення елементів масиву" << std::endl;
+    std::cout << "2) Вивести значення елементів масиву" << std::endl;
+    std::cout << "3) Обчислення мінімального елементу" << std::endl;
+    std::cout << "4) Обчислення максимального елементу" << std::endl;
+    std::cout << "5) Обчислення середнього арифметичного" << std::endl;
+    std::cout << "6) Відсортувати масив за зростанням" << std::endl;
+    std::cout << "7) Відсортувати масив за спаданням" << std::endl;
+    std::cout << "8) Вийти" << std::endl;
     while(true){
-        std::cout << "Оберіть операцію:" << std::endl;
-        std::cout << "1) Ввести значення елементів масиву" << std::endl;
-        std::cout << "2) Вивести значення елементів масиву" << std::endl;
-        std::cout << "3) Обчислення мінімального елементу" << std::endl;
-        std::cout << "4) Обчислення максимального елементу" << std::endl;
-        std::cout << "5) Обчислення середнього арифметичного" << std::endl;
-        std::cout << "6) Відсортувати масив за зростанням" << std::endl;
-        std::cout << "7) Відсортувати масив за спаданням" << std::endl;
-        std::cout << "8) Вийти" << std::endl;
+        std::cout << std::endl <<"Оберіть операцію (1-8): ";
         std::cin >> chose;
         if (chose != '8'){
             choseOperation(chose, isCreate, arr);
@@ -40,7 +41,7 @@ int* createArr(int* arr){
         std::cout << "Елемент " << i << ": ";
         std::cin >> arr[i];
     }
-    std::cout << "Масив на " << ARR_LENGHT << " елементів(0-" << ARR_LENGHT-1 << ") створено!" << std::endl;
+    std::cout << "Масив на " << ARR_LENGHT << " елементів (0-" << ARR_LENGHT-1 << ") створено!" << std::endl;
     return arr;
 }
 
@@ -139,7 +140,7 @@ void choseOperation(char chose, bool &isCreate, int* arr){
         }
     case '5':
         if (isCreate){
-            std::cout << "Середнє арифметичне по елементах масиву" << midlOfEelems(arr) << std::endl;
+            std::cout << "Середнє арифметичне по елементах масиву: " << midlOfEelems(arr) << std::endl;
             break;
         }
         else{
