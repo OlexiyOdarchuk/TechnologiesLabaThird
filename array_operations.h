c072613cb7e2f58b31a4af813d26a6830ed6938d
iShawyha <shawyhaf@gmail.com>
2025-09-29 18:56:01 +0300

    Готова лабораторна. Можна звісно доробити ще в інших виглядах це реалізувати, але лєнь шось якось

---
+++ array_operations.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+
+constexpr int ARR_LENGTH = 5;
+
+void createArr(int arr[ARR_LENGTH]);
+void writeElements(const int arr[ARR_LENGTH]);
+int minElemArr(const int arr[ARR_LENGTH]);
+int maxElemArr(const int arr[ARR_LENGTH]);
+double calculateAverage(const int arr[ARR_LENGTH]);
+void sortArrMin(int arr[ARR_LENGTH]);
+void sortArrMax(int arr[ARR_LENGTH]);
+bool requireCreated(bool isCreate);
+void chooseOperation(const char choose, bool &isCreate, int arr[ARR_LENGTH]);
